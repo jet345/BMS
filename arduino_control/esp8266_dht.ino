9975f51954e9694a664e0688946bcfc69698337b
jet345 <rudal1994@naver.com>
2017-08-11 13:41:08 +0000

    reset

---
+++ arduino_control/esp8266_dht.ino
@@ -0,0 +1,168 @@
+#include <DHT.h>
+#include <WiFiEspClient.h>
+#include <WiFiEsp.h>
+#include <WiFiEspUdp.h>
+#include <PubSubClient.h>
+#include "SoftwareSerial.h"
+
+#define WIFI_AP "iptime_kem"
+#define WIFI_PASSWORD "38053267"
+
+// DHT
+#define DHTPIN 4
+#define DHTTYPE DHT11
+
+char myServer[] = "168.192.0.5";
+
+const char *mqtt_server = "m20.cloudmqtt.com";
+const int mqtt_port = 17132;
+const char *mqtt_user = "ej";
+const char *mqtt_pass = "1234";
+const char *mqtt_client_name = "arduino1"; // Client connections cant have the same connection name
+
+// Initialize the Ethernet client object
+WiFiEspClient espClient;
+
+// Initialize DHT sensor.
+DHT dht(DHTPIN, DHTTYPE);
+
+//PubSubClient client(espClient);
+PubSubClient client(mqtt_server, mqtt_port, espClient);
+SoftwareSerial soft(2, 3); // RX, TX
+
+int status = WL_IDLE_STATUS;
+unsigned long lastSend;
+
+void setup() {
+  // initialize serial for debugging
+  Serial.begin(9600);
+  soft.begin(9600);
+  dht.begin();
+  InitWiFi();
+  //client.setServer( myServer, 1883 );
+  lastSend = 0;
+}
+
+void loop() {
+  status = WiFi.status();
+  if ( status != WL_CONNECTED) {
+    while ( status != WL_CONNECTED) {
+      Serial.print("Attempting to connect to WPA SSID: ");
+      Serial.println(WIFI_AP);
+      // Connect to WPA/WPA2 network
+      status = WiFi.begin(WIFI_AP, WIFI_PASSWORD);
+      delay(500);
+    }
+    Serial.println("Connected to AP");
+  }
+
+  if (WiFi.status() == WL_CONNECTED) {
+    if (!client.connected()) {
+      Serial.println("Connecting to MQTT server");
+      if (client.connect(mqtt_client_name,mqtt_user,mqtt_pass)) {
+        Serial.println("Connected to MQTT server");
+        client.subscribe("Arduino1/DHT");
+      } else {
+        Serial.println("Could not connect to MQTT server");   
+      }
+    }
+   }
+
+  if ( !client.connected() ) {
+    reconnect();
+  }
+
+  if ( millis() - lastSend > 1000 ) { // Update and send only after 1 seconds
+    getAndSendTemperatureAndHumidityData();
+    lastSend = millis();
+  }
+
+  client.loop();
+}
+
+void getAndSendTemperatureAndHumidityData()
+{
+  Serial.println("Collecting temperature data.");
+
+  // Reading temperature or humidity takes about 250 milliseconds!
+  float h = dht.readHumidity();
+  // Read temperature as Celsius (the default)
+  float t = dht.readTemperature();
+
+  // Check if any reads failed and exit early (to try again).
+  if (isnan(h) || isnan(t)) {
+    Serial.println("Failed to read from DHT sensor!");
+    return;
+  }
+
+  Serial.print("Humidity: ");
+  Serial.print(h);
+  Serial.print(" %\t");
+  Serial.print("Temperature: ");
+  Serial.print(t);
+  Serial.print(" *C ");
+
+  String temperature = String(t);
+  String humidity = String(h);
+
+
+  // Just debug messages
+  Serial.print( "Sending temperature and humidity : [" );
+  Serial.print( temperature ); Serial.print( "," );
+  Serial.print( humidity );
+  Serial.print( "]   -> " );
+
+  // Prepare a JSON payload string
+  String payload = "{";
+  payload += "\"temperature\":"; payload += temperature; payload += ",";
+  payload += "\"humidity\":"; payload += humidity;
+  payload += "}";
+
+  // Send payload
+  char attributes[100];
+  payload.toCharArray( attributes, 100 );
+  client.publish( "Arduino1/DHT", attributes );
+  Serial.println( attributes );
+}
+
+void InitWiFi()
+{
+  // initialize serial for ESP module
+  soft.begin(9600);
+  // initialize ESP module
+  WiFi.init(&soft);
+  // check for the presence of the shield
+  if (WiFi.status() == WL_NO_SHIELD) {
+    Serial.println("WiFi shield not present");
+    // don't continue
+    while (true);
+  }
+
+  Serial.println("Connecting to AP ...");
+  // attempt to connect to WiFi network
+  while ( status != WL_CONNECTED) {
+    Serial.print("Attempting to connect to WPA SSID: ");
+    Serial.println(WIFI_AP);
+    // Connect to WPA/WPA2 network
+    status = WiFi.begin(WIFI_AP, WIFI_PASSWORD);
+    delay(500);
+  }
+  Serial.println("Connected to AP");
+}
+
+void reconnect() {
+  // Loop until we're reconnected
+  while (!client.connected()) {
+    Serial.print("Connecting to server node ...");
+    // Attempt to connect (clientId, username, password)
+    if ( client.connect("Arduino Uno Device") ) {
+      Serial.println( "[DONE]" );
+    } else {
+      Serial.print( "[FAILED] [ rc = " );
+      Serial.print( client.state() );
+      Serial.println( " : retrying in 5 seconds]" );
+      // Wait 5 seconds before retrying
+      delay( 5000 );
+    }
+  }
+}
