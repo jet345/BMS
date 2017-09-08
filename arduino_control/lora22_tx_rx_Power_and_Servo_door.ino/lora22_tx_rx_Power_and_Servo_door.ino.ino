28dbb7f2fde362ab1c5d9887f40f61b4554d4ea6
kim subin <tnqls7019@naver.com>
2017-09-09 01:13:20 +0900

    Update lora22_tx_rx_Power_and_Servo_door.ino.ino

--- arduino_control/lora22_tx_rx_Power_and_Servo_door.ino/lora22_tx_rx_Power_and_Servo_door.ino.ino
+++ arduino_control/lora22_tx_rx_Power_and_Servo_door.ino/lora22_tx_rx_Power_and_Servo_door.ino.ino
@@ -1,73 +1,76 @@
 #include <arduinoLoRa.h>
-#include <arduinoUtils.h>
-#include <math.h>
-#include <Wire.h>
 #include <SPI.h>
-#include "ACS712.h"
+#include <Wire.h>
 #include <Servo.h>
 
 unsigned long lastSend_r;
+unsigned long lastSend_i;
 int e;
 String tmp ="";
-char message[20];
-char my_packet[100];
-int n;
+char my_packet[20];
 
-//Power sensor and Servo
-ACS712 sensor(ACS712_30A, A0);
-Servo myservo;
 
-int pos = 0;
+Servo myservo;  // create servo object to control a servo
+// twelve servo objects can be created on most boards
+
+int pos = 0;    // variable to store the servo position
 
 void setup() {
-  //servo and power sensor
   myservo.attach(9);  // attaches the servo on pin 9 to the servo object
-  sensor.calibrate();
+
   
-  Serial.begin(9600);
   //LoRa
+  // Open serial communications and wait for port to open:
+  Serial.begin(9600);
+
   // Print a start message
-  Serial.println(F("SX1272 module and Arduino: send packets without ACK"));
+  Serial.println(F("SX1272 module and Arduino: receive packets without ACK"));
+
   // Power ON the module
   e = sx1272.ON();
   Serial.print(F("Setting power ON: state "));
-  Serial.println(e, DEC);  
+  Serial.println(e, DEC);
+  
   // Set transmission mode and print the result
-  e |= sx1272.setMode(4);
+  e = sx1272.setMode(4);
   Serial.print(F("Setting Mode: state "));
-  Serial.println(e, DEC);  
+  Serial.println(e, DEC);
+  
   // Set header
-  e |= sx1272.setHeaderON();
+  e = sx1272.setHeaderON();
   Serial.print(F("Setting Header ON: state "));
-  Serial.println(e, DEC);  
+  Serial.println(e, DEC);
+  
   // Select frequency channel
-  e |= sx1272.setChannel(CH_10_868);
+  e = sx1272.setChannel(CH_10_868);
   Serial.print(F("Setting Channel: state "));
-  Serial.println(e, DEC); 
+  Serial.println(e, DEC);
+  
   // Set CRC
-  e |= sx1272.setCRC_ON();
+  e = sx1272.setCRC_ON();
   Serial.print(F("Setting CRC ON: state "));
-  Serial.println(e, DEC);  
+  Serial.println(e, DEC);
+  
   // Select output power (Max, High or Low)
-  e |= sx1272.setPower('H');
+  e = sx1272.setPower('H');
   Serial.print(F("Setting Power: state "));
-  Serial.println(e, DEC);  
+  Serial.println(e, DEC);
+  
   // Set the node address and print the result
-  e |= sx1272.setNodeAddress(8);
+  e = sx1272.setNodeAddress(8);
   Serial.print(F("Setting node address: state "));
-  Serial.println(e, DEC);  
+  Serial.println(e, DEC);
+  
   // Print a success message
-  if (e == 0)
-    Serial.println(F("SX1272 successfully configured"));
-  else
-    Serial.println(F("SX1272 initialization failed"));   
+  Serial.println(F("SX1272 successfully configured"));
+  Serial.println();
 
 }
 
 void loop() {
-  int ascii;
-   
-  e = sx1272.receivePacketTimeout(1000);
+
+   // Receive message
+  e = sx1272.receivePacketTimeout(10000);
   if ( e == 0 )
   {
     Serial.print(F("Receive packet, state "));
@@ -76,12 +79,9 @@ void loop() {
     for (unsigned int i = 0; i < sx1272.packet_received.length; i++)
     {
       my_packet[i] = (char)sx1272.packet_received.data[i];
-    }
-    Serial.print(F("Message: "));
-    Serial.println(my_packet);
-    ascii=my_packet[0];
-    //Door
-      if( ascii==111 ) { //시리얼 모니터로 O 주면 열림
+
+      //servo 
+     if(my_packet=='o') { //시리얼 모니터로 O 주면 열림
         Serial.println("open");
         for (pos = 35; pos <= 120; pos += 1) { // goes from 0 degrees to 180 degrees
           // in steps of 1 degree
@@ -90,34 +90,26 @@ void loop() {
         }
         delay(2000);//문열려있는시간
         
-        Serial.print("wait...");       
+        Serial.print("wait...");/*
+        for (pos = 180; pos <= 0; pos -= 1) { // goes from 0 degrees to 180 degrees
+        // in steps of 1 degree
+          myservo.write(pos);              // tell servo to go to position in variable 'pos'
+          delay(15);                       // waits 15ms for the servo to reach the position
+        }*/
         Serial.println("close");
         myservo.write(35); //자동으로 문닫기         
-      }    
+      }/*
+      else if(my_packet=='X'){ //X주면 위험알려줌
+          Serial.println("DANGER");
+          delay(2000);
+      }*/
+    }
+    Serial.print(F("Message: "));
+    Serial.println(my_packet);
   }
-  delay(100);
-  //Serial.print("** mills - lastSend_r : ");
-  //Serial.println(millis()-lastSend_r);
-  if (millis() - lastSend_r > 60000) { //1분 마다
-
-    //Power sensor
-    float U = 230;
-    float I = sensor.getCurrentAC();
-    float P = U * I;
-    
-    tmp += String(P);
-    tmp += "/";
-    tmp += " ";
-  
-    tmp.toCharArray(message, 20);
-    e = sx1272.sendPacketTimeout(8, message);
-    tmp = "";
-    
-    Serial.print(F("Packet sent, state "));
-    Serial.println(message);
-    message[0] = '\0';
-    lastSend_r=millis();
-   
+  else {
+    Serial.print(F("Receive packet, state "));
+    Serial.println(e, DEC);
   }
-  delay(100);
+    
 }
